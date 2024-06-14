e214eb7e17c6710a8c1312397c81b6570dd1d8d8
mariaaoprea <marita.oprea@gmail.com>
2024-06-14 17:33:18 +0200

    simplified code

--- preprocessing.py
+++ preprocessing.py
@@ -1,10 +1,8 @@
 from datasets import load_dataset
 import os
-import random
 import torch
 from transformers import CLIPTokenizer
 from torchvision import transforms
-import numpy as np
 from arguments import parse_args
 
 args = parse_args()
@@ -12,61 +10,26 @@ tokenizer = CLIPTokenizer.from_pretrained(
     args.pretrained_model_name_or_path, subfolder="tokenizer"
 )
 
-DATASET_NAME_MAPPING = {
-    "foorplan": ("image", "text"),
-}
 
-if args.dataset_name is not None:
-    dataset = load_dataset(
-        args.dataset_name,
-        args.dataset_config_name,
-        cache_dir=args.cache_dir,
-        data_dir=args.train_data_dir,
-    )
-else:
-    data_files = {}
-    if args.train_data_dir is not None:
-        data_files["train"] = os.path.join(args.train_data_dir, "**")
-    dataset = load_dataset(
-        "imagefolder",
-        data_files=data_files,
-        cache_dir=args.cache_dir,
-    )
+# Load dataset
+data_files = {}
+data_files["train"] = os.path.join(args.train_data_dir, "**")
+dataset = load_dataset(
+    "imagefolder",
+    data_files=data_files,
+    cache_dir=args.cache_dir,
+)
 
-#get the column names for input/target
+# Get the column names
 column_names = dataset["train"].column_names
-dataset_columns = DATASET_NAME_MAPPING.get(args.dataset_name, None)
-if args.image_column is None:
-    print("IMAGE COLUMN NONE")
-    image_column = dataset_columns[0] if dataset_columns is not None else column_names[0]
-else:
-    image_column = args.image_column
-    if image_column not in column_names:
-        raise ValueError(
-            f"--image_column' value '{args.image_column}' needs to be one of: {', '.join(column_names)}"
-        )
-if args.caption_column is None:
-    caption_column = dataset_columns[1] if dataset_columns is not None else column_names[1]
-else:
-    caption_column = args.caption_column
-    if caption_column not in column_names:
-        raise ValueError(
-            f"--caption_column' value '{args.caption_column}' needs to be one of: {', '.join(column_names)}"
-        )
-#DATASET_NAME_MAPPING = {"foorplan": ("image", "text"),}
+image_column = column_names[0]
+caption_column = column_names[1]
 
-#tokenize input captions and transform the images
+# Tokenize input captions and transform the images
 def tokenize_captions(examples, is_train=True):
     captions = []
     for caption in examples[caption_column]:
-        if isinstance(caption, str):
-            captions.append(caption)
-        elif isinstance(caption, (list, np.ndarray)):
-            captions.append(random.choice(caption) if is_train else caption[0])
-        else:
-            raise ValueError(
-                f"Caption column `{caption_column}` should contain either strings or lists of strings."
-            )
+        captions.append(caption)
     inputs = tokenizer(
         captions, max_length=tokenizer.model_max_length, padding="max_length", truncation=True, return_tensors="pt"
     )
@@ -74,8 +37,6 @@ def tokenize_captions(examples, is_train=True):
 
 train_transforms = transforms.Compose(
     [
-        transforms.Resize(args.resolution, interpolation=transforms.InterpolationMode.BILINEAR),
-        transforms.CenterCrop(args.resolution) if args.center_crop else transforms.RandomCrop(args.resolution),
         transforms.RandomHorizontalFlip() if args.random_flip else transforms.Lambda(lambda x: x),
         transforms.ToTensor(),
         transforms.Normalize([0.5], [0.5]),
@@ -95,19 +56,18 @@ def collate_fn(examples):
     return {"pixel_values": pixel_values, "input_ids": input_ids}
 
 def preprocess_data(accelerator):
-
     with accelerator.main_process_first():
-        if args.max_train_samples is not None:
-            dataset["train"] = dataset["train"].shuffle(seed=args.seed).select(range(args.max_train_samples))
+        dataset["train"] = dataset["train"].shuffle(seed=args.seed)
         train_dataset = dataset["train"].with_transform(preprocess_train)
 
-    #create the dataloader
+    # Create the dataloader
     train_dataloader = torch.utils.data.DataLoader(
         train_dataset,
         shuffle=True,
         collate_fn=collate_fn,
-        batch_size=args.train_batch_size,
-        num_workers=args.dataloader_num_workers,
+        batch_size=args.train_batch_size
     )
-
     return train_dataloader
+
+
+
