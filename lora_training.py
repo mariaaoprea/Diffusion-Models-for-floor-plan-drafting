c6e6eb1194f38a2bbceaef1f0d0c1c276f4b1651
mariaaoprea <marita.oprea@gmail.com>
2024-06-14 17:31:41 +0200

    added comments, removed unnecessary features

--- lora_training.py
+++ lora_training.py
@@ -31,6 +31,7 @@ from diffusers.optimization import get_scheduler
 from diffusers.training_utils import compute_snr
 from diffusers.utils import check_min_version, convert_state_dict_to_diffusers, is_wandb_available
 from diffusers.utils.torch_utils import is_compiled_module
+import wandb
 
 # Additional imports for SSIM and LPIPS
 from skimage.metrics import structural_similarity as ssim
@@ -44,25 +45,22 @@ from preprocessing import preprocess_data
 check_min_version("0.28.0.dev0")
 
 def main():
+    # Parse the arguments
     args = parse_args()
-
+    # Initialize the logger
     logger = get_logger(__name__, log_level="INFO")
     logging_dir = Path(args.output_dir, args.logging_dir)
     accelerator_project_config = ProjectConfiguration(project_dir=args.output_dir, logging_dir=logging_dir)
-
+    # Initialize the accelerator
     accelerator = Accelerator(
-        gradient_accumulation_steps=args.gradient_accumulation_steps,
         log_with=args.report_to,
         project_config=accelerator_project_config
     )
-
+    # Load the preprocessed dataset
     train_dataloader = preprocess_data(accelerator)
 
-    if args.report_to == "wandb":
-        if not is_wandb_available():
-            raise ImportError("Make sure to install wandb if you want to use it for logging during training.")
-        import wandb
-        wandb.init(project="stable-diffusion-lora")
+    # Initialize wandb project    
+    wandb.init(project="stable-diffusion-lora")
 
     # Make one log on every process with the configuration for debugging.
     logging.basicConfig(
@@ -76,7 +74,7 @@ def main():
     transformers.utils.logging.set_verbosity_warning()
     diffusers.utils.logging.set_verbosity_info()
 
-    # If passed along, set the training seed now.
+    # Set the training seed for reproductible training
     if args.seed is not None:
         set_seed(args.seed)
 
@@ -101,13 +99,13 @@ def main():
     unet.requires_grad_(False)
     vae.requires_grad_(False)
     text_encoder.requires_grad_(False)
-
+    # Set the weight dtype
     weight_dtype = torch.float32
 
     # Freeze the unet parameters before adding adapters
     for param in unet.parameters():
         param.requires_grad_(False)
-
+    # Define the LoraConfig
     unet_lora_config = LoraConfig(
         r=args.rank,
         lora_alpha=args.rank,
@@ -122,35 +120,16 @@ def main():
 
     # Add adapter 
     unet.add_adapter(unet_lora_config)
-
+    # Unfreeze the lora layers
     lora_layers = filter(lambda p: p.requires_grad, unet.parameters())
-
-    if args.gradient_checkpointing:
-        unet.enable_gradient_checkpointing()
-
-    if args.scale_lr:
-        args.learning_rate = (
-            args.learning_rate * args.gradient_accumulation_steps * args.train_batch_size * accelerator.num_processes
-        )
-    
+    # Unwrap the model if it is a compiled module
     def unwrap_model(model):
         model = accelerator.unwrap_model(model)
         model = model._orig_mod if is_compiled_module(model) else model
         return model
 
-    # Initialize the optimizer
-    if args.use_8bit_adam:
-        try:
-            import bitsandbytes as bnb
-        except ImportError:
-            raise ImportError(
-                "Please install bitsandbytes to use 8-bit Adam. You can do so by running `pip install bitsandbytes`"
-            )
-
-        optimizer_cls = bnb.optim.AdamW8bit
-    else:
-        optimizer_cls = torch.optim.AdamW
-
+    # Initialize the optimizer - Use AdamW
+    optimizer_cls = torch.optim.AdamW
     optimizer = optimizer_cls(
         lora_layers,
         lr=args.learning_rate,
@@ -159,18 +138,16 @@ def main():
         eps=args.adam_epsilon,
     )
 
-    # Scheduler and math around the number of training steps.
-    overrode_max_train_steps = False
-    num_update_steps_per_epoch = math.ceil(len(train_dataloader) / args.gradient_accumulation_steps)
-    if args.max_train_steps is None:
-        args.max_train_steps = args.num_train_epochs * num_update_steps_per_epoch
-        overrode_max_train_steps = True
+    # Calculate the number of update steps per epoch
+    num_update_steps_per_epoch = len(train_dataloader)
+    train_steps = args.num_train_epochs * num_update_steps_per_epoch
 
+    # Initialize the learning rate scheduler
     lr_scheduler = get_scheduler(
         args.lr_scheduler,
         optimizer=optimizer,
-        num_warmup_steps=args.lr_warmup_steps * accelerator.num_processes,
-        num_training_steps=args.max_train_steps * accelerator.num_processes,
+        num_warmup_steps=args.lr_warmup_steps,
+        num_training_steps=train_steps
     )
 
     # Prepare everything with our `accelerator`.
@@ -181,27 +158,15 @@ def main():
     # Initialize LPIPS function
     lpips_fn = lpips.LPIPS(net='alex').to(accelerator.device)
 
-    # We need to recalculate our total training steps as the size of the training dataloader may have changed.
-    num_update_steps_per_epoch = math.ceil(len(train_dataloader) / args.gradient_accumulation_steps)
-    if overrode_max_train_steps:
-        args.max_train_steps = args.num_train_epochs * num_update_steps_per_epoch
-    # Afterwards we recalculate our number of training epochs
-    args.num_train_epochs = math.ceil(args.max_train_steps / num_update_steps_per_epoch)
-
-    # We need to initialize the trackers we use, and also store our configuration.
-    # The trackers initializes automatically on the main process.
+    # Initialize the trackers
     if accelerator.is_main_process:
         accelerator.init_trackers("floorplan-fine-tune", config=vars(args))
 
-    # Train!
-    total_batch_size = args.train_batch_size * accelerator.num_processes * args.gradient_accumulation_steps
-
+    # Initialize the training loop
     logger.info("***** Running training *****")
     logger.info(f"  Num Epochs = {args.num_train_epochs}")
-    logger.info(f"  Instantaneous batch size per device = {args.train_batch_size}")
-    logger.info(f"  Total train batch size (w. parallel, distributed & accumulation) = {total_batch_size}")
-    logger.info(f"  Gradient Accumulation steps = {args.gradient_accumulation_steps}")
-    logger.info(f"  Total optimization steps = {args.max_train_steps}")
+    logger.info(f"  Batch size = {args.train_batch_size}")
+    logger.info(f"  Total optimization steps = {train_steps}")
     global_step = 0
     first_epoch = 0
 
@@ -232,14 +197,14 @@ def main():
     else:
         initial_global_step = 0
 
+    # Initialize the progress bar
     progress_bar = tqdm(
-        range(0, args.max_train_steps),
+        range(0, train_steps),
         initial=initial_global_step,
-        desc="Steps",
-        # Only show the progress bar once on each machine.
-        disable=not accelerator.is_local_main_process,
+        desc="Steps"
     )
 
+    # Training loop
     for epoch in range(first_epoch, args.num_train_epochs):
         unet.train()
         for step, batch in enumerate(train_dataloader):
@@ -248,31 +213,21 @@ def main():
                 latents = vae.encode(batch["pixel_values"].to(dtype=weight_dtype)).latent_dist.sample()
                 latents = latents * vae.config.scaling_factor
 
-                # Sample noise that we'll add to the latents
+                # Generate noise for the forward diffusion process
                 noise = torch.randn_like(latents)
-                if args.noise_offset:
-                    # https://www.crosslabs.org//blog/diffusion-with-offset-noise
-                    noise += args.noise_offset * torch.randn(
-                        (latents.shape[0], latents.shape[1], 1, 1), device=latents.device
-                    )
 
-                bsz = latents.shape[0]
                 # Sample a random timestep for each image
-                timesteps = torch.randint(0, noise_scheduler.config.num_train_timesteps, (bsz,), device=latents.device)
+                timesteps = torch.randint(0, noise_scheduler.config.num_train_timesteps, (latents.shape[0],), device=latents.device)
                 timesteps = timesteps.long()
 
+                # Forward diffusion process
                 # Add noise to the latents according to the noise magnitude at each timestep
-                # (this is the forward diffusion process)
                 noisy_latents = noise_scheduler.add_noise(latents, noise, timesteps)
 
                 # Get the text embedding for conditioning
                 encoder_hidden_states = text_encoder(batch["input_ids"], return_dict=False)[0]
 
                 # Get the target for loss depending on the prediction type
-                if args.prediction_type is not None:
-                    # set prediction_type of scheduler if defined
-                    noise_scheduler.register_to_config(prediction_type=args.prediction_type)
-
                 if noise_scheduler.config.prediction_type == "epsilon":
                     target = noise
                 elif noise_scheduler.config.prediction_type == "v_prediction":
@@ -321,26 +276,6 @@ def main():
 
                 if global_step % args.checkpointing_steps == 0:
                     if accelerator.is_main_process:
-                        # _before_ saving state, check if this save would set us over the `checkpoints_total_limit`
-                        if args.checkpoints_total_limit is not None:
-                            checkpoints = os.listdir(args.output_dir)
-                            checkpoints = [d for d in checkpoints if d.startswith("checkpoint")]
-                            checkpoints = sorted(checkpoints, key=lambda x: int(x.split("-")[1]))
-
-                            # before we save the new checkpoint, we need to have at _most_ `checkpoints_total_limit - 1` checkpoints
-                            if len(checkpoints) >= args.checkpoints_total_limit:
-                                num_to_remove = len(checkpoints) - args.checkpoints_total_limit + 1
-                                removing_checkpoints = checkpoints[0:num_to_remove]
-
-                                logger.info(
-                                    f"{len(checkpoints)} checkpoints already exist, removing {len(removing_checkpoints)} checkpoints"
-                                )
-                                logger.info(f"removing checkpoints: {', '.join(removing_checkpoints)}")
-
-                                for removing_checkpoint in removing_checkpoints:
-                                    removing_checkpoint = os.path.join(args.output_dir, removing_checkpoint)
-                                    shutil.rmtree(removing_checkpoint)
-
                         save_path = os.path.join(args.output_dir, f"checkpoint-{global_step}")
                         accelerator.save_state(save_path)
 
@@ -360,29 +295,26 @@ def main():
             logs = {"step_loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0]}
             progress_bar.set_postfix(**logs)
 
-            if global_step >= args.max_train_steps:
-                break
-
         # Compute SSIM and LPIPS once per epoch
         unet.eval()
         with torch.no_grad():
             recon_images = vae.decode(latents).sample
             target_images = vae.decode(target).sample
 
-            # Compute SSIM directly on PyTorch tensors using batched operations
+            # Compute SSIM 
             ssim_value = 0.0
             for i in range(recon_images.shape[0]):
                 ssim_value += ssim(recon_images[i].permute(1, 2, 0).cpu().numpy(), target_images[i].permute(1, 2, 0).cpu().numpy(), multichannel=True, win_size=7,  channel_axis=-1,data_range=1.0)
             ssim_value /= recon_images.shape[0]
 
-            # Compute LPIPS directly on GPU
+            # Compute LPIPS
             lpips_value = lpips_fn(recon_images, target_images).mean().item()
 
-        # Log SSIM and LPIPS values
         accelerator.log({
             "ssim": ssim_value,
-            "lpips": lpips_value
-        }, step=epoch)
+            "lpips": lpips_value,
+            "epoch": epoch
+        }, step=global_step)
 
         if accelerator.is_main_process:
             if args.validation_prompt is not None and epoch % args.validation_epochs == 0:
@@ -404,11 +336,10 @@ def main():
                 if args.seed is not None:
                     generator = generator.manual_seed(args.seed)
                 images = []
-                if torch.backends.mps.is_available():
-                    autocast_ctx = nullcontext()
-                else:
-                    autocast_ctx = torch.autocast(accelerator.device.type)
+                
+                autocast_ctx = torch.autocast(accelerator.device.type)
 
+                # Generate images
                 with autocast_ctx:
                     for _ in range(args.num_validation_images):
                         images.append(
