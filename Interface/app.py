2aa2742c5d70b81e34902918cfa80769bd3f025d
mariaaoprea <marita.oprea@gmail.com>
2024-07-11 10:52:01 +0200

    refactor: added documentation for functions

--- Interface/app.py
+++ Interface/app.py
@@ -18,6 +18,20 @@ pipeline = AutoPipelineForText2Image.from_pretrained("runwayml/stable-diffusion-
 pipeline.load_lora_weights("checkpoint-15000", weight_name="pytorch_lora_weights.safetensors")
 
 def generate_images(prompt, num_images, scheduler, inference_steps, task_id):
+    """
+    Generate a specified number of images based on a given prompt using a diffusion model.
+
+    Args:
+        prompt (str): The text prompt used to generate the images.
+        num_images (int): The number of images to generate.
+        scheduler (str): The name of the scheduler to use for the diffusion model.
+        inference_steps (int): The number of inference steps to perform for each image.
+        task_id (str): The ID of the task associated with the image generation.
+
+    Returns:
+        list: A list of generated images in base64-encoded PNG format.
+
+    """
     # Set scheduler 
     if scheduler == "DDIM":
         pipeline.scheduler = DDIMScheduler.from_pretrained("runwayml/stable-diffusion-v1-5", subfolder="scheduler")  # You might need to map the scheduler name to the actual scheduler class
@@ -53,17 +67,45 @@ def generate_images(prompt, num_images, scheduler, inference_steps, task_id):
     return images
 
 class Task:
+    """
+    Represents a task that can be added and run.
+
+    Attributes:
+        tasks (dict): A dictionary to store the tasks.
+        results (dict): A dictionary to store the results of the tasks.
+        task_number (int): A counter to keep track of the task number.
+
+    Methods:
+        add_task: Adds a new task to the tasks dictionary.
+        run: Executes the tasks and generates the results.
+    """
+
     def __init__(self):
         self.tasks = {}
         self.results = {}
         self.task_number = 0
 
     def add_task(self, prompt, num_images, scheduler, inference_steps):
+        """
+        Adds a new task to the tasks dictionary.
+
+        Args:
+            prompt (str): The prompt for the task.
+            num_images (int): The number of images to generate.
+            scheduler (str): The scheduler to use for image generation.
+            inference_steps (int): The number of inference steps to perform.
+
+        Returns:
+            int: The task number assigned to the new task.
+        """
         self.task_number += 1
         self.tasks[self.task_number] = (prompt, num_images, scheduler, inference_steps)
         return self.task_number
 
     def run(self):
+        """
+        Executes the tasks and generates the results.
+        """
         while True:
             done_tasks = []
             for task_number, (prompt, num_images, scheduler, inference_steps) in self.tasks.items():
@@ -76,10 +118,31 @@ class Task:
 
 @app.route('/', methods=['GET'])
 def home():
+    """
+    Renders the 'index.html' template.
+
+    Returns:
+        The rendered 'index.html' template.
+    """
     return render_template('index.html')
 
 @app.route('/submit', methods=['POST'])
 def submit():
+    """
+    Submits a task with the provided data.
+
+    Parameters:
+    - data (dict): A dictionary containing the following keys:
+        - 'prompt' (str): The prompt for the task.
+        - 'num_images' (int): The number of images for the task.
+        - 'scheduler' (str): The scheduler for the task.
+        - 'inference_steps' (int): The number of inference steps for the task.
+
+    Returns:
+    - response (json): A JSON response containing the task ID if the task was successfully added,
+      or an error message if any of the required data is missing.
+
+    """
     data = request.json
     if 'prompt' in data and 'num_images' in data and 'scheduler' in data and 'inference_steps' in data:
         task_id = task.add_task(data['prompt'], data['num_images'], data['scheduler'], data['inference_steps'])
@@ -89,6 +152,18 @@ def submit():
 
 @app.route('/status/<task_id>')
 def status(task_id):
+    """
+    Returns the status of a task based on the given task_id.
+
+    Args:
+        task_id (int): The ID of the task.
+
+    Returns:
+        A JSON response containing the status of the task. If the task is found in `task.tasks`, 
+        the status will be "processing". If the task is found in `task.results`, the status will 
+        be "done" along with the URLs of the results. If the task is not found, the status will 
+        be "not found" along with a 404 status code.
+    """
     task_id = int(task_id)
     if task_id in task.tasks:
         return jsonify({"status": "processing"})
@@ -99,10 +174,22 @@ def status(task_id):
 
 @socketio.on('connect', namespace='/generate')
 def handle_connect():
+    """
+    Handles the connection of a client.
+
+    This function is called when a client connects to the server. It prints a message indicating that a client has connected.
+
+    """
     print('Client connected')
 
 @socketio.on('disconnect', namespace='/generate')
 def handle_disconnect():
+    """
+    Handles the disconnection of a client.
+
+    This function is called when a client disconnects from the server. It prints a message indicating that the client has disconnected.
+
+    """
     print('Client disconnected')
 
 if __name__ == '__main__':
