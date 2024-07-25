af50bb6696ed57e7dfb5fa3e5706cb359dca1d1c
mariaaoprea <marita.oprea@gmail.com>
2024-07-25 19:13:21 +0200

    fix: Correct parsing of prompts

--- Evaluation/Robustness/image_generation.py
+++ Evaluation/Robustness/image_generation.py
@@ -5,7 +5,7 @@ import torch
 pipeline = AutoPipelineForText2Image.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16).to("cuda")
 
 # Load the LoRA weights for the model
-pipeline.load_lora_weights("Checkpoint_L1/checkpoint-250", weight_name="pytorch_lora_weights.safetensors")
+pipeline.load_lora_weights("Checkpoints_L1/checkpoint-250", weight_name="pytorch_lora_weights.safetensors")
 
 # Define a list of prompts for generating floor plan images
 prompts = [
@@ -63,12 +63,12 @@ prompts = [
 labels = ["p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8"]
 
 # Generate and save the floor plan images for each prompt and label
-for prompt, label in zip(prompts, labels):
-    for i in range(1, 5):
+for i, prompt in enumerate(prompts):
+    for j in range(1,6):
         # Generate the image using the pipeline
         image = pipeline(prompt).images[0]
         
         # Save the generated image with the corresponding label and index
-        image.save(f"Evaluation/Robustness/images/{label}_{i}_{i%5}.png")
+        image.save(f"Evaluation/Robustness/images/{i%8}_{i%5}_{j}.png")
 
 
