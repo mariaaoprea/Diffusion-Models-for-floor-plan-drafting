f242006b4930d52cb20ff13ada053d1d3680f5b3
Maria Oprea <103122570+mariaaoprea@users.noreply.github.com>
2024-08-27 12:55:22 +0200

    Update and rename app-selection_input.py to app.py

--- Interface/app.py
+++ Interface/app.py
@@ -19,7 +19,7 @@ logging.basicConfig(level=logging.INFO)
 # Initialize the model
 device = "cuda" if torch.cuda.is_available() else "cpu"
 pipeline = AutoPipelineForText2Image.from_pretrained("runwayml/stable-diffusion-v1-5").to(device)
-pipeline.load_lora_weights("checkpoint-15000", weight_name="pytorch_lora_weights.safetensors")
+pipeline.load_lora_weights("model", weight_name="pytorch_lora_weights.safetensors") #add path to model
 
 def generate_images(prompt, num_images, scheduler, inference_steps, task_id):
     """
