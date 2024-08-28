5ae8458c343c4be14c3a003d43d99a79529c222e
Maria Oprea <marita.oprea@gmail.com>
2024-08-28 17:52:05 +0200

    Refactor

--- Evaluation/Training Loss/plot_different_losses.py
+++ Evaluation/Training Loss/plot_different_losses.py
@@ -14,24 +14,60 @@ def add_trend_line(ax, x, y, color):
 # Create the plot for the combined losses with trend lines
 fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(18, 6), sharex=True)
 
+<<<<<<< HEAD
+<<<<<<< HEAD
+# Plotting L1_loss moving average and trend line
+ax1.plot(data['Epoch'], data['L1_loss_avg'], label=f'L1_loss (Moving Average, window={window_size})', color='red')
+add_trend_line(ax1, data['Epoch'], data['L1_loss_avg'], color='grey')
+=======
 # Plotting L1_loss
 ax1.plot(data['Epoch'], data['L1_loss'], label='L1_loss', color='red')
 add_trend_line(ax1, data['Epoch'], data['L1_loss'], color='grey')
+>>>>>>> parent of 00fbc96 (Refactor)
+=======
+# Plotting L1_loss
+ax1.plot(data['Epoch'], data['L1_loss'], label='L1_loss', color='red')
+add_trend_line(ax1, data['Epoch'], data['L1_loss'], color='grey')
+>>>>>>> parent of 00fbc96 (Refactor)
 ax1.set_title('L1 Loss')
 ax1.set_xlabel('Epoch')
 ax1.set_ylabel('Loss')
 ax1.grid(True)
 
+<<<<<<< HEAD
+<<<<<<< HEAD
+# Plotting MSE_loss moving average and trend line
+ax2.plot(data['Epoch'], data['MSE_loss_avg'], label=f'MSE_loss (Moving Average, window={window_size})', color='gold')
+add_trend_line(ax2, data['Epoch'], data['MSE_loss_avg'], color='grey')
+=======
 # Plotting MSE_loss
 ax2.plot(data['Epoch'], data['MSE_loss'], label='MSE_loss', color='gold')
 add_trend_line(ax2, data['Epoch'], data['MSE_loss'], color='grey')
+>>>>>>> parent of 00fbc96 (Refactor)
+=======
+# Plotting MSE_loss
+ax2.plot(data['Epoch'], data['MSE_loss'], label='MSE_loss', color='gold')
+add_trend_line(ax2, data['Epoch'], data['MSE_loss'], color='grey')
+>>>>>>> parent of 00fbc96 (Refactor)
 ax2.set_title('MSE Loss')
 ax2.set_xlabel('Epoch')
 ax2.grid(True)
 
+<<<<<<< HEAD
+<<<<<<< HEAD
+# Plotting SNR_loss moving average and trend line
+ax3.plot(data['Epoch'], data['SNR_loss_avg'], label=f'SNR_loss (Moving Average, window={window_size})', color='blue')
+add_trend_line(ax3, data['Epoch'], data['SNR_loss_avg'], color='grey')
+=======
 # Plotting SNR_loss
 ax3.plot(data['Epoch'], data['SNR_loss'], label='SNR_loss', color='blue')
 add_trend_line(ax3, data['Epoch'], data['SNR_loss'], color='grey')
+>>>>>>> parent of 00fbc96 (Refactor)
+=======
+# Plotting SNR_loss
+ax3.plot(data['Epoch'], data['SNR_loss'], label='SNR_loss', color='blue')
+add_trend_line(ax3, data['Epoch'], data['SNR_loss'], color='grey')
+>>>>>>> parent of 00fbc96 (Refactor)
 ax3.set_title('SNR Loss')
 ax3.set_xlabel('Epoch')
 ax3.grid(True)
